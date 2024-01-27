e20003b2edd1e43776372fb018b71ac94063f125
Xir <prssergey@gmail.com>
2024-01-27 13:59:04 +0300

    layout for user page. rerenders?

--- src/app/user/[userName]/backlogs/[[...list]]/page.tsx
+++ src/app/user/[userName]/backlogs/[[...list]]/page.tsx
@@ -4,8 +4,6 @@ import Feed from "@/containers/Feed";
 import GameBacklog from "@/containers/GameBacklog";
 import { useParams } from "next/navigation";
 import React from "react";
-import { backlogs } from "../../../../../../mock/data";
-import NavItem from "@/components/NavItem";
 
 export default function Lists() {
   const params = useParams();
@@ -26,21 +24,7 @@ export default function Lists() {
   return (
     <main className="flex w-full  flex-col items-center">
       {JSON.stringify(params)}
-      <div className="flex w-full max-w-5xl flex-col p-4">
-        <div className=" flex w-full justify-between">
-          <div className="flex">
-            {backlogs.map((backlog) => (
-              <NavItem
-                key={backlog.title}
-                href={`/user/${params.userName}/backlogs/${backlog.title}`}
-                label={backlog.title}
-              />
-            ))}
-          </div>
-          <button>Create Backlog</button>
-        </div>
-        {isActiveBacklog ? renderBacklog(params.list[0]) : <Feed />}
-      </div>
+      {isActiveBacklog ? renderBacklog(params.list[0]) : <Feed />}
     </main>
   );
 }

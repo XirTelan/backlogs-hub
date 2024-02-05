597b13990fcf1492854a8fef94d411306e2122d4
Xir <prssergey@gmail.com>
2024-02-05 19:08:33 +0300

    feat: react-hot-toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 import NavBar from "@/containers/NavBar";
 import Footer from "@/containers/Footer";
@@ -25,6 +26,17 @@ export default function RootLayout({
     <html lang="en">
       <ClerkProvider>
         <body className={`${roboto.className} flex flex-col items-center `}>
+          <Toaster
+            toastOptions={{
+              className: "",
+              style: {
+                borderRadius: "5px",
+                background: "#222",
+                color: "#fff",
+                border: "solid 2px #333",
+              },
+            }}
+          />
           <NavBar />
           <main className="container flex flex-col justify-center">
             {children}

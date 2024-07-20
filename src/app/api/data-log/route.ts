4271f4a6a31ddddd3469eb779a63b7657b5dae04
Xir <prssergey@gmail.com>
2024-07-20 18:44:27 +0300

    fix: ???

--- src/app/api/data-log/route.ts
+++ src/app/api/data-log/route.ts
@@ -1,11 +1,11 @@
-import { sendContactForm } from "@/services/log";
+import { createLogEntry } from "@/services/dataLog";
 import { sendMsg } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
   try {
-    const res = await sendContactForm(data);
+    const res = await createLogEntry(data);
     if (res.isSuccess) return NextResponse.json("Success", { status: 200 });
   } catch (error) {
     return sendMsg.error(error);

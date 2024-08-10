970922000c8a5332dc69d3594bee6dc0296d7097
Xir <prssergey@gmail.com>
2024-08-10 14:31:20 +0300

    refactor: modal 1. options for diff btn positions 2. provide props for customize actions btn

--- src/components/Common/Modal.tsx
+++ src/components/Common/Modal.tsx
@@ -3,52 +3,66 @@ import { createPortal } from "react-dom";
 import ButtonBase, { ButtonColorVariants } from "./UI/ButtonBase";
 import { ButtonBaseProps } from "@/types";
 
+const DEFAULTS: ModalProps["actionOptions"] = {
+  position: "inherit",
+  align: "bottom",
+};
+
 const Modal = ({
   action,
-  actionType = "primary",
   confirmOptions,
   showActions,
+  actionOptions = DEFAULTS,
   setClose,
   children,
-}: {
-  confirmOptions?: ButtonBaseProps;
-  action?: () => unknown;
-  actionType?: ButtonColorVariants;
-  showActions?: boolean;
-  setClose: () => void;
-  children: React.ReactElement | React.ReactElement[];
-}) => {
+}: ModalProps) => {
+  const positions = {
+    inherit: "",
+    absolute: "absolute top-0 z-50",
+  };
+  const ActionsBlock = () => (
+    <div
+      className={`flex w-full ${positions[actionOptions.position ?? DEFAULTS.position!]}`}
+    >
+      <ButtonBase
+        style={{ width: "100%" }}
+        variant={actionOptions.cancelBtn?.clrVariant ?? "secondary"}
+        text={actionOptions.cancelBtn?.text ?? "Cancel"}
+        onClick={setClose}
+      />
+      {action && (
+        <ButtonBase
+          style={{ width: "100%" }}
+          variant={actionOptions.confirmBtn?.clrVariant ?? "primary"}
+          text={actionOptions.confirmBtn?.text ?? "confirm"}
+          {...confirmOptions}
+          onClick={async () => {
+            if (!action) return;
+            action();
+            setClose();
+          }}
+        />
+      )}
+    </div>
+  );
+
   return createPortal(
     <div
       onClick={setClose}
-      className="absolute inset-0 z-50 flex items-center justify-center bg-black/60"
+      className="absolute inset-0 z-50 flex items-center justify-center bg-black/60 "
     >
       <div
         className="absolute  flex flex-col justify-center"
         onClick={(e) => e.stopPropagation()}
       >
-        <>{children}</>
-        {showActions && (
-          <div className="flex w-full">
-            <ButtonBase
-              style={{ width: "100%" }}
-              variant="secondary"
-              text="Cancel"
-              onClick={setClose}
-            />
-            <ButtonBase
-              style={{ width: "100%" }}
-              variant={actionType}
-              text="Confirm"
-              {...confirmOptions}
-              onClick={async () => {
-                if (!action) return;
-                action();
-                setClose();
-              }}
-            />
-          </div>
+        {showActions && (actionOptions.align ?? DEFAULTS.align) == "top" && (
+          <ActionsBlock />
         )}
+        <>{children}</>
+        {showActions &&
+          (actionOptions.align ?? DEFAULTS.align) === "bottom" && (
+            <ActionsBlock />
+          )}
       </div>
     </div>,
     document.body,
@@ -56,3 +70,23 @@ const Modal = ({
 };
 
 export default Modal;
+
+type ModalProps = {
+  confirmOptions?: ButtonBaseProps;
+  action?: () => unknown;
+  showActions?: boolean;
+  actionOptions?: {
+    position?: "inherit" | "absolute";
+    align?: "top" | "bottom";
+    confirmBtn?: {
+      text?: string;
+      clrVariant?: ButtonColorVariants;
+    };
+    cancelBtn?: {
+      text?: string;
+      clrVariant?: ButtonColorVariants;
+    };
+  };
+  setClose: () => void;
+  children: React.ReactElement | React.ReactElement[];
+};

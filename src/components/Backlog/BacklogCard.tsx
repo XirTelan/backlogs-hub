bfb8465e27cd4e5d428bdf2e97205302ea778bad
Xir <prssergey@gmail.com>
2024-08-16 17:44:19 +0300

    style: BacklogCard adjustments for large screen.

--- src/components/Backlog/BacklogCard.tsx
+++ src/components/Backlog/BacklogCard.tsx
@@ -1,25 +1,20 @@
-import { LinkProps } from "next/link";
-import React, { AnchorHTMLAttributes } from "react";
+import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import LinkWithBtnStyle from "../Common/UI/LinkWithBtnStyle";
 import { IoAdd } from "react-icons/io5";
 
-const BacklogCard = ({
-  children,
-  href,
-  createLink,
-  ...props
-}: BacklogCardProps) => {
+const BacklogCard = ({ children, href, createLink }: BacklogCardProps) => {
   return (
-    <div className="group flex h-52 w-40 flex-shrink-0 flex-col  bg-layer-1  hover:bg-field-hover-1 ">
+    <div className="group relative flex h-52 w-40 flex-shrink-0 flex-col  bg-layer-1  hover:bg-field-hover-1 ">
       <div className="p-4">
         <>{children}</>
       </div>
-      <div className="mt-auto flex opacity-100 transition-all duration-300 ease-in-out *:w-full md:flex-wrap md:opacity-0 md:group-hover:opacity-100 ">
+      <div className="mt-auto flex opacity-100 transition-all duration-300 ease-in-out *:w-full md:absolute md:inset-0 md:m-0 md:flex-col-reverse  md:flex-wrap md:bg-layer-1 md:opacity-0 md:group-hover:opacity-100 ">
         <LinkWithBtnStyle
           size="small"
           variant="ghostAccent"
-          href={href}
+          href={createLink}
+          className="md:h-1/4"
           icon={<IoAdd size={24} />}
         >
           Add Item
@@ -27,7 +22,8 @@ const BacklogCard = ({
         <LinkWithBtnStyle
           size="small"
           variant="tertiary"
-          href={createLink}
+          href={href}
+          className="md:h-3/4"
           icon={<FaArrowRight />}
         >
           View

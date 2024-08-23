c8c6a89eeb90462f76209a1258055970a87ba410
Xir <prssergey@gmail.com>
2024-08-23 18:51:47 +0300

    fix: update with new Table

--- src/containers/Backlogs/UserFieldsBlock.tsx
+++ src/containers/Backlogs/UserFieldsBlock.tsx
@@ -3,11 +3,19 @@ import React from "react";
 import { useFieldArray, useWatch } from "react-hook-form";
 import { FieldsBlockProps } from "@/types";
 import ButtonBase from "@/components/Common/UI/ButtonBase";
-import TableBase from "@/lib/createTable";
 import Title from "@/components/Common/Title";
 import FieldsArrayItem from "./FieldsArrayItem";
 import { Field } from "@/zodTypes";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import createTable from "@/lib/createTable";
+
+const Table = createTable();
+const headers = [
+  { id: "index", title: "#", width: "1rem" },
+  { id: "name", title: "Name", width: "auto" },
+  { id: "type", title: "Type" },
+  { id: "actions", title: <BsThreeDotsVertical />, width: "48px" },
+];
 
 const UserFieldsBlock = ({ errors, control, register }: FieldsBlockProps) => {
   const fieldsArray = useFieldArray({
@@ -20,6 +28,7 @@ const UserFieldsBlock = ({ errors, control, register }: FieldsBlockProps) => {
     name: "fields",
     control,
   });
+
   return (
     <section>
       <Title variant={3} style={{ margin: ".5rem 0" }} title={"Fields"}>
@@ -38,52 +47,52 @@ const UserFieldsBlock = ({ errors, control, register }: FieldsBlockProps) => {
           }
         />
       </Title>
-      <TableBase
-        headers={[
-          { id: "index", title: "#", width: "1rem" },
-          { id: "name", title: "Name", width: "auto" },
-          { id: "type", title: "Type" },
-          { id: "actions", title: <BsThreeDotsVertical />, width: "48px" },
-        ]}
-        description={""}
-      >
-        <>
-          <tr className=" border-b border-subtle-1">
-            <td colSpan={2} className="px-4">
-              <InputField value={"Title"} variant="small" layer={2} readOnly />
-            </td>
-            <td className="px-4 text-secondary-text" colSpan={2}>
-              This field is required and cannot be deleted
-            </td>
-          </tr>
-          {fields.map((field, index) => (
-            <FieldsArrayItem
-              key={field._id ?? field.id ?? index}
-              index={index}
-              register={register}
-              field={field}
-              error={errors && errors[index]}
-              remove={fieldsArray.remove}
-              customAction={{
-                update: () => {
-                  if (field.type !== "select") return;
-                  fieldsArray.update(index, {
-                    ...field,
-                    data: field.data ? [...field.data, ""] : [""],
-                  });
-                },
-                remove: () => {
-                  if (field.type !== "select") return;
-                  fieldsArray.update(index, {
-                    ...field,
-                    data: [...field.data.slice(0, -1)],
-                  });
-                },
-              }}
-            />
-          ))}
-        </>
-      </TableBase>
+      <Table.Wrap>
+        <Table.Head headers={headers}></Table.Head>
+        <Table.Body>
+          <>
+            <tr className=" border-b border-subtle-1">
+              <td colSpan={2} className="px-4">
+                <InputField
+                  value={"Title"}
+                  variant="small"
+                  layer={2}
+                  readOnly
+                />
+              </td>
+              <td className="px-4 text-secondary-text" colSpan={2}>
+                This field is required and cannot be deleted
+              </td>
+            </tr>
+            {fields.map((field, index) => (
+              <FieldsArrayItem
+                key={field._id ?? field.id ?? index}
+                index={index}
+                register={register}
+                field={field}
+                error={errors && errors[index]}
+                remove={fieldsArray.remove}
+                customAction={{
+                  update: () => {
+                    if (field.type !== "select") return;
+                    fieldsArray.update(index, {
+                      ...field,
+                      data: field.data ? [...field.data, ""] : [""],
+                    });
+                  },
+                  remove: () => {
+                    if (field.type !== "select") return;
+                    fieldsArray.update(index, {
+                      ...field,
+                      data: [...field.data.slice(0, -1)],
+                    });
+                  },
+                }}
+              />
+            ))}
+          </>
+        </Table.Body>
+      </Table.Wrap>
     </section>
   );
 };

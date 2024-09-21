600e2027b26a3255851ced274877d90208632a80
Xir <prssergey@gmail.com>
2024-09-21 15:15:05 +0300

    feat: sidepanel onhover opening

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useOutsideClickReg from "@/hooks/useOutsideClickReg";
-import React, { useRef, useState } from "react";
+import useToggle from "@/hooks/useToggle";
+import React, { useEffect, useRef } from "react";
 
 const buttonPosStyles = {
   left: "border-e",
@@ -9,7 +10,7 @@ const buttonPosStyles = {
 };
 const dropDownStyle = {
   left: "left-0 border-r",
-  none: "right-0 border-r border-l",
+  none: "right-0 border",
   right: "right-0 border-l",
 };
 
@@ -24,19 +25,20 @@ const SidePanel = ({
   children,
   borders = true,
   keepOpen = false,
+  onHover = false,
 }: {
   position?: "left" | "right" | "none";
   icon: React.ReactNode;
   borders?: boolean;
   children: React.ReactNode | React.ReactNode[];
   keepOpen?: boolean;
+  onHover?: boolean;
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { isOpen, setOpen, setClose, toggle } = useToggle(false);
   const ref = useRef<HTMLDivElement>(null);
 
-  useOutsideClickReg(isOpen, ref, () => {
-    setIsOpen(false);
-  });
+  useOutsideClickReg(isOpen, ref, setClose);
+
   const styleDropDownPos = dropDownStyle[position];
   const styleButtonPos = buttonPosStyles[position];
   const styleButton = borders
@@ -51,22 +53,29 @@ const SidePanel = ({
     if (keepOpen) e.stopPropagation();
   };
 
+  useEffect(() => {
+    if (!onHover) return;
+    if (!ref.current) return;
+
+    ref.current.addEventListener("mouseenter", setOpen);
+    ref.current.addEventListener("mouseleave", setClose);
+
+    () => {
+      ref.current?.removeEventListener("mouseenter", setOpen);
+      ref.current?.removeEventListener("mouseleave", setClose);
+    };
+  }, [setClose, setOpen, onHover]);
+
   return (
-    <div
-      ref={ref}
-      onClick={() => {
-        setIsOpen((prev) => !prev);
-      }}
-      className="relative"
-    >
+    <div ref={ref} onClick={toggle} className={` relative`}>
       <button
-        className={`${styleButton} ${position === "none" ? "h-12" : "h-[49px]"}  w-12   p-[14px] hover:bg-subtle-3/15`}
+        className={`${styleButton} ${position === "none" ? "h-12" : "h-[49px]"}  w-12   p-[14px]  hover:bg-subtle-3/15`}
       >
         {icon}
       </button>
       {isOpen && (
         <div
-          className={`${styleDropDownPos} absolute top-full z-50 w-64  border-b  border-border-1 bg-background   `}
+          className={`${styleDropDownPos} absolute top-full z-10 w-64 border-b  border-border-1  bg-background py-2   `}
           onClick={handleClosePanel}
         >
           <>{children}</>

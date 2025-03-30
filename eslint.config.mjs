6b7a0d3754fdb4c7799bf8af7c4f6f03eea58025
Xir <prssergey@gmail.com>
2025-03-31 01:30:46 +0300

    update pckg, configs.

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked
+  ),
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    extends: compat.extends("next/core-web-vitals", "next/typescript"),
+  },
+]);

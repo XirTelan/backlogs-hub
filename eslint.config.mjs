cbbe73d6faf0ef8a56d2dee33e493c93a3c375c0
Xir <prssergey@gmail.com>
2025-04-10 20:24:04 +0300

    fix: incorrect work of eslint vscode extension with next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,16 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
-export default defineConfig([
-  tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked
-  ),
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    extends: compat.extends("next/core-web-vitals", "next/typescript"),
-  },
-]);
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
